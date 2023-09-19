9d063ab48a7ea9ed1656e6e07db5d3dc56259b9c
greemoid <greemoiddd@gmail.com>
2023-09-19 13:49:52 +0300

    add colors for design, dependencies, splashscreen

--- app/src/main/java/com/example/englishwritinginviews/presentation/MainFragment.kt
+++ app/src/main/java/com/example/englishwritinginviews/presentation/MainFragment.kt
@@ -1,8 +1,6 @@
 package com.example.englishwritinginviews.presentation
 
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
-import com.example.englishwritinginviews.R
 import com.example.englishwritinginviews.databinding.FragmentMainBinding
 import com.example.englishwritinginviews.presentation.core.BaseFragment
 
@@ -12,11 +10,7 @@ class MainFragment :
     override val viewModel: EmptyViewModel by viewModels()
 
     override fun init() {
-        binding.text.text = "1231231"
 
-        binding.text.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_listOfQuestionsFragment)
-        }
     }
 
 
--- app/src/main/java/com/example/englishwritinginviews/presentation/core/MainActivity.kt
+++ app/src/main/java/com/example/englishwritinginviews/presentation/core/MainActivity.kt
@@ -2,14 +2,14 @@ package com.example.englishwritinginviews.presentation.core
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import com.example.englishwritinginviews.R
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         val host: NavHostFragment = supportFragmentManager
