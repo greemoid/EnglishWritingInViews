d17772d812e37ac9eed4d3dda26fcf9fd18a024c
greemoid <greemoiddd@gmail.com>
2023-10-11 16:37:03 +0300

    changed minSdk to 26, calendar is done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.englishwritinginviews"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/example/englishwritinginviews/presentation/strike/StrikeFragment.kt
+++ app/src/main/java/com/example/englishwritinginviews/presentation/strike/StrikeFragment.kt
@@ -1,10 +1,8 @@
 package com.example.englishwritinginviews.presentation.strike
 
 import android.annotation.SuppressLint
-import android.os.Build
 import android.view.View
 import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.core.view.children
 import com.example.englishwritinginviews.R
 import com.example.englishwritinginviews.databinding.CalendarDayLayoutBinding
@@ -29,12 +27,15 @@ class StrikeFragment :
 
     private val monthCalendarView: CalendarView get() = binding.calendarView
 
-    private val selectedDates = mutableSetOf<LocalDate>()
+    private val selectedDates = mutableSetOf<LocalDate>(
+        LocalDate.of(2023, 10, 13),
+        LocalDate.of(2023, 10, 14),
+        LocalDate.of(2023, 10, 16),
+        LocalDate.of(2023, 10, 19)
+    )
 
-    @RequiresApi(Build.VERSION_CODES.O)
     private val today = LocalDate.now()
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun init() {
 
         val daysOfWeek = daysOfWeek()
@@ -42,50 +43,13 @@ class StrikeFragment :
             .map { it as TextView }
             .forEachIndexed { index, textView ->
                 textView.text = daysOfWeek[index].displayText()
-                textView.setTextColor(resources.getColor(R.color.red_button, context?.theme))
+                textView.setTextColor(resources.getColor(R.color.hint_color, context?.theme))
             }
 
         val currentMonth = YearMonth.now()
         val startMonth = currentMonth.minusMonths(100)
         val endMonth = currentMonth.plusMonths(100)
         setupMonthCalendar(startMonth, endMonth, currentMonth, daysOfWeek)
-        /* binding.calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
-             override fun bind(container: DayViewContainer, data: CalendarDay) {
-                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                     container.textView.text = data.date.dayOfMonth.toString()
-                     if (data.date.dayOfMonth % 2 == 0) {
-                         container.imageView.setImageDrawable(
-                             ContextCompat.getDrawable(
-                                 requireContext(),
-                                 R.drawable.poo,
-                             )
-                         )
-                     } else {
-                         container.imageView.setImageDrawable(
-                             ContextCompat.getDrawable(
-                                 requireContext(),
-                                 R.drawable.ok,
-                             )
-                         )
-                     }
-                 } else {
-                     container.textView.text = "1"
-                 }
-             }
-
-             override fun create(view: View): DayViewContainer = DayViewContainer(view)
-
-         }
-
-         val currentMonth = YearMonth.now()
-         val startMonth = currentMonth.minusMonths(100)  // Adjust as needed
-         val endMonth = currentMonth.plusMonths(100)  // Adjust as needed
-         val firstDayOfWeek = firstDayOfWeekFromLocale() // Available from the library
-         setupMonthCalendar(startMonth, endMonth, currentMonth, firstDayOfWeek)
-         binding.calendarView.scrollToMonth(currentMonth)
-         binding.calendarView.outDateStyle = OutDateStyle.EndOfRow*/
-
-
     }
 
     private fun setupMonthCalendar(
@@ -95,45 +59,37 @@ class StrikeFragment :
         daysOfWeek: List<DayOfWeek>,
     ) {
         class DayViewContainer(view: View) : ViewContainer(view) {
-            // Will be set when this container is bound. See the dayBinder.
             lateinit var day: CalendarDay
             val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
-
-            /*init {
-                view.setOnClickListener {
-                    if (day.position == DayPosition.MonthDate) {
-                        dateClicked(date = day.date)
-                    }
-                }
-            }*/
         }
+
         monthCalendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
             override fun create(view: View) = DayViewContainer(view)
 
-            @RequiresApi(Build.VERSION_CODES.O)
             override fun bind(container: DayViewContainer, data: CalendarDay) {
                 container.day = data
                 firstDayOfWeekFromLocale()
                 bindDate(data.date, container.textView, data.position == DayPosition.MonthDate)
             }
         }
-//        monthCalendarView.monthScrollListener = { updateTitle() }
+
+        monthCalendarView.monthScrollListener = { updateTitle() }
         monthCalendarView.setup(startMonth, endMonth, daysOfWeek.first())
         monthCalendarView.scrollToMonth(currentMonth)
     }
 
 
-    @RequiresApi(Build.VERSION_CODES.O)
     private fun bindDate(date: LocalDate, textView: TextView, isSelectable: Boolean) {
         textView.text = date.dayOfMonth.toString()
         if (isSelectable) {
             when {
                 selectedDates.contains(date) -> {
-                    textView.setTextColor(resources.getColor(R.color.red_button, context?.theme))
+                    textView.setTextColor(resources.getColor(R.color.white_text, context?.theme))
+                    textView.setBackgroundResource(R.drawable.green_textview_background)
                 }
 
                 today == date -> {
-                    textView.setTextColor(resources.getColor(R.color.white, context?.theme))
+                    textView.setTextColor(resources.getColor(R.color.red_button, context?.theme))
                 }
 
                 else -> {
@@ -141,40 +97,26 @@ class StrikeFragment :
                 }
             }
         } else {
-            textView.setTextColor(resources.getColor(R.color.light_red_button, context?.theme))
+            textView.setTextColor(resources.getColor(R.color.hint_color, context?.theme))
             textView.background = null
         }
     }
 
-    /*@SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n")
     private fun updateTitle() {
         val month = monthCalendarView.findFirstVisibleMonth()?.yearMonth ?: return
-        binding.exOneYearText.text = month.year.toString()
-        binding.exOneMonthText.text = month.month.displayText(short = false)
-    }*/
+        binding.tvYear.text = month.year.toString()
+        binding.tvMonth.text = month.month.displayText(short = false)
+    }
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
-fun YearMonth.displayText(short: Boolean = false): String {
-    return "${this.month.displayText(short = short)} ${this.year}"
-}
-
-@RequiresApi(Build.VERSION_CODES.O)
 fun Month.displayText(short: Boolean = true): String {
     val style = if (short) TextStyle.SHORT else TextStyle.FULL
     return getDisplayName(style, Locale.ENGLISH)
 }
 
-@RequiresApi(Build.VERSION_CODES.O)
 fun DayOfWeek.displayText(uppercase: Boolean = false): String {
     return getDisplayName(TextStyle.SHORT, Locale.ENGLISH).let { value ->
         if (uppercase) value.uppercase(Locale.ENGLISH) else value
     }
-}
-
-class DayViewContainer(view: View) : ViewContainer(view) {
-
-    val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
-//    val imageView = CalendarDayLayoutBinding.bind(view).imageView
-
 }
